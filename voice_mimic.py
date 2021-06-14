6edaf31d63e8ea29022c8959ada12ca842690898
lixavi <121849896+lixavi@users.noreply.github.com>
2021-06-14 11:12:52 +0530

    updated

--- voice_mimic.py
+++ voice_mimic.py
@@ -19,12 +19,4 @@ class VoiceMimic:
         model.to(self.device)
         return model
 
-    def load_weights(self, weights_path):
-        self.model.load_state_dict(torch.load(weights_path, map_location=self.device))
-        self.model.eval()
-
-    def synthesize_voice(self, input_sequence):
-        with torch.no_grad():
-            input_sequence = input_sequence.to(self.device)
-            output = self.model.generate(input_sequence)
-        return output
+ 
