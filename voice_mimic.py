d6233d8e78c57c8b116b829984380d81f83ad6e8
lixavi <121849896+lixavi@users.noreply.github.com>
2021-06-18 11:13:19 +0530

    updated

--- voice_mimic.py
+++ voice_mimic.py
@@ -7,17 +7,7 @@ class VoiceMimic:
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.model = self._init_model()
 
-    def _init_model(self):
-        model = WaveNet(num_classes=self.config.num_classes,
-                        num_layers=self.config.num_layers,
-                        num_blocks=self.config.num_blocks,
-                        kernel_size=self.config.kernel_size,
-                        residual_channels=self.config.residual_channels,
-                        dilation_channels=self.config.dilation_channels,
-                        skip_channels=self.config.skip_channels,
-                        output_channels=self.config.output_channels)
-        model.to(self.device)
-        return model
+
 
     def load_weights(self, weights_path):
         self.model.load_state_dict(torch.load(weights_path, map_location=self.device))
