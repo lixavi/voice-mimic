8688cab97a50562c9a6cd5a35731addfe6df082b
lixavi <121849896+lixavi@users.noreply.github.com>
2021-07-18 11:18:14 +0530

    updated

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-from config import config
-from voice_mimic import VoiceMimic
-from train import train
-from synthesize import synthesize
 
-def main():
-    # Initialize VoiceMimic system
-    voice_mimic = VoiceMimic(config)
 
     # Train the WaveNet model (optional)
     # train(voice_mimic.model, train_data, config.sequence_length, config.batch_size, config.num_epochs, config.learning_rate)
