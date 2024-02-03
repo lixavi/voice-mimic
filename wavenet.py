798f448afbc1f3a4f148e3fec31e4068b1045b01
AA <121849896+lixavi@users.noreply.github.com>
2024-02-03 12:56:13 +0530

    updated

---
+++ wavenet.py
@@ -0,0 +1,65 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, dilation):
+        super(ResidualBlock, self).__init__()
+        self.conv_dilated = nn.Conv1d(in_channels, out_channels, kernel_size, padding=dilation, dilation=dilation)
+        self.conv_residual = nn.Conv1d(in_channels, out_channels, kernel_size=1)
+        self.conv_skip = nn.Conv1d(out_channels, out_channels, kernel_size=1)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        output = self.relu(self.conv_dilated(x) + self.conv_residual(x))
+        skip = self.conv_skip(output)
+        return output, skip
+
+class WaveNet(nn.Module):
+    def __init__(self, num_classes, num_layers, num_blocks, kernel_size, residual_channels, dilation_channels, skip_channels, output_channels):
+        super(WaveNet, self).__init__()
+        self.num_classes = num_classes
+        self.num_layers = num_layers
+        self.num_blocks = num_blocks
+        self.kernel_size = kernel_size
+        self.residual_channels = residual_channels
+        self.dilation_channels = dilation_channels
+        self.skip_channels = skip_channels
+        self.output_channels = output_channels
+        
+        self.input_conv = nn.Conv1d(num_classes, residual_channels, kernel_size=1)
+        self.conv_blocks = nn.ModuleList()
+        for _ in range(num_blocks):
+            for i in range(num_layers):
+                dilation = 2 ** i
+                self.conv_blocks.append(ResidualBlock(residual_channels, dilation_channels, kernel_size, dilation))
+        self.skip_conv = nn.Conv1d(dilation_channels, skip_channels, kernel_size=1)
+        self.relu = nn.ReLU()
+        self.output_conv1 = nn.Conv1d(skip_channels, output_channels, kernel_size=1)
+        self.output_conv2 = nn.Conv1d(output_channels, num_classes, kernel_size=1)
+
+    def forward(self, x):
+        x = self.input_conv(x)
+        skip_connections = []
+        for block in self.conv_blocks:
+            x, skip = block(x)
+            skip_connections.append(skip)
+        skip_sum = sum(skip_connections)
+        x = self.relu(skip_sum)
+        x = self.skip_conv(x)
+        x = self.relu(x)
+        x = self.output_conv1(x)
+        x = self.relu(x)
+        x = self.output_conv2(x)
+        return x
+
+    def generate(self, input_sequence, length=1000):
+        with torch.no_grad():
+            generated_sequence = torch.zeros((input_sequence.shape[0], self.num_classes, length)).to(input_sequence.device)
+            generated_sequence[:, :, :input_sequence.shape[2]] = input_sequence
+            for i in range(input_sequence.shape[2], length):
+                input_slice = generated_sequence[:, :, i - self.kernel_size + 1:i]
+                output_slice = self.forward(input_slice)
+                predicted_sample = F.softmax(output_slice[:, :, -1], dim=1)
+                generated_sequence[:, :, i] = predicted_sample
+            return generated_sequence
